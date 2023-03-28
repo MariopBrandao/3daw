b897c509b8f65b57ca617603f837eca5894efd86
Mario Brandao <127356019+MariopBrandao@users.noreply.github.com>
2023-03-28 10:32:02 -0300

    Add files via upload

---
+++ 3DawTeste28marco.php
@@ -0,0 +1,45 @@
+   <?php
+    $nomes = array("jose","neusa","antonio","erika","tiago","chico");
+    $notas = array(7.5,7.0,7.1,8.0,8.5,7.7);
+    echo $nome[0];
+    echo "<br>";
+    echo $nome[1];
+    echo "<br>";
+    echo $nome[2];
+    echo "<br>";
+    echo $nome[3];
+    echo "<br>";
+    echo $nome[4];
+    echo "<br>";
+?>
+<html>
+  <head>
+    <title>3 Daw</title>
+  </head>
+  <body>
+    <h1>array<h1>
+    <table>
+      <tr>
+        <th>Nome<th>
+        <th>Nota<th>
+      <tr>
+        <?php
+            for ($x=0; $x<=5; $x++){
+        ?>
+              <tr>
+
+
+                
+              </tr>
+            }
+
+        
+echo '<p>As Arvres Samos Nozes</p>'; ?> 
+    <!--
+    This script places a badge on your repl's full-browser view back to your repl's cover
+    page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
+    teal, blue, blurple, magenta, pink!
+    -->
+    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+  </body>
+</html>
